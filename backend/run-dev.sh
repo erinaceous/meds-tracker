6af1771e7596e8d581e3813e39dbd0679c52435e
Owain Jones <owanes@noc.ac.uk>
2024-07-30 00:44:11 +0100

    backend: install psycopg2-binary

--- backend/run-dev.sh
+++ backend/run-dev.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 source venv/bin/activate
 pip install -r requirements.txt
+source .env
 fastapi dev src/meds_tracker/api.py
--- backend/src/meds_tracker/persistent_db.py
+++ backend/src/meds_tracker/persistent_db.py
@@ -8,11 +8,7 @@ persistent_db = create_engine(settings.persistent_db_uri)
 
 
 def init_local_persistent_db(recreate=False):
-    if data_dir not in settings.persistent_db_uri:
-        return
     os.makedirs(data_dir, exist_ok=True)
-    if recreate is False and os.path.isfile(persistent_db_path):
-        return
     # SQLModel.metadata.drop_all(persistent_db.engine)
     from .models.altcha import Altcha
     from .models.report import Report
